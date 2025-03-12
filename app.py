74bbc8e52cf6efba2fac5169c3b01461d065cdd6
Zade Mahayni <zmahayni056@gmail.com>
2025-03-12 12:47:01 -0500

    made upload button + download excel sheet backend

--- app.py
+++ app.py
@@ -1,20 +1,68 @@
 from flask import Flask
-from flask import render_template, request, jsonify, send_file
+from flask import render_template, request, redirect, url_for, flash, send_file
 from flask_cors import CORS
 import os
 import pandas as pd
 
 # from flask_restful import 
 app = Flask(__name__)
-CORS(app)
+app.secret_key="supersecretkey"
 
-uploads = "uploads"
-os.makedirs(uploads, exist_ok=True)
-app.config["Upload Folder"] = uploads
+os.makedirs("uploads", exist_ok=True)
+app.config["UPLOAD_FOLDER"] = "uploads"
 
-@app.route('/', methods=["POST"])
-def home():
-    return render_template("LandingPage.html")
+saved_files= []
 
-if __name__ == '__main__': 
-    app.run(debug=True, port=5001)
+@app.route('/', methods=["GET", "POST"])
+def upload_files():
+    excel_ready = False
+    if request.method == 'POST':
+        if "files" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        
+        files = request.files.getlist("files")
+
+        global saved_files
+        saved_files.clear()
+
+        for file in files:
+            filename = file.filename
+            print(f"Processing {filename}")
+            if file.filename and not file.filename.startswith("."):
+                file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                try:
+                    file.save(file_path)
+                    saved_files.append(file.filename)
+                except FileNotFoundError:
+                    flash(f"Error saving file: {file.filename}. Skipping.")
+                    continue
+
+        if saved_files:
+            excel_path = os.path.join(app.config["UPLOAD_FOLDER"], "file_list.xlsx")
+            df = pd.DataFrame(saved_files, columns=["Uploaded Files"])
+            df.to_excel(excel_path, index=False)
+
+
+            flash("Files uploaded successfully! Click below to download the Excel file.")
+            excel_ready = True
+            return render_template("LandingPage.html", excel_ready=excel_ready)
+
+    return render_template("LandingPage.html", excel_ready=excel_ready)
+
+@app.route("/download")
+def download_file():
+    excel_path = os.path.join(app.config["UPLOAD_FOLDER"], "file_list.xlsx")
+    if os.path.exists(excel_path):
+        response = send_file(excel_path, as_attachment=True)
+        os.remove(excel_path)
+        global saved_files
+        saved_files.clear()
+        
+        return response
+    else:
+        return redirect(url_for("upload_files"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
